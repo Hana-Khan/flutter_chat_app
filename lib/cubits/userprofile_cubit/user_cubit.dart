d6b7c34e776b489778771ab13a052033cf74dd87
Muhammad Musa <musaakhaan22@gmail.com>
2021-08-11 12:58:26 +0500

    Merge branch 'master' of https://github.com/Hana-Khan/flutter_chat_app
    # Conflicts:
    #	lib/main.dart
    #	lib/presentation/screens/authentication.dart

--- lib/cubits/userprofile_cubit/user_cubit.dart
+++ lib/cubits/userprofile_cubit/user_cubit.dart
@@ -1,15 +1,46 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_chat_app/cubits/userprofile_cubit/user_state.dart';
-import 'package:flutter_chat_app/data_repo/repositories/user_repository.dart';
 
-class UserCubit extends Cubit<UserCubit> {
-  
+abstract class UserListener {
+  void usersLoaded();
+  void userInfoLoaded();
+  void failToLoadUser();
+  void userUpdated();
+  void userDeleted();
+  void noUsers();
+}
+
+class UserCubit extends Cubit<UserCubit> implements UserListener {
   UserCubit(UserCubit initialState) : super(initialState);
-    // final UserRepository repository;
 
+  getListOfUsers() {}
 
-  
+  @override
+  void failToLoadUser() {
+    // TODO: implement failToLoadUser
+  }
 
+  @override
+  void noUsers() {
+    // TODO: implement noUsers
+  }
 
+  @override
+  void userDeleted() {
+    // TODO: implement userDeleted
+  }
 
+  @override
+  void userInfoLoaded() {
+    // TODO: implement userInfoLoaded
+  }
+
+  @override
+  void userUpdated() {
+    // TODO: implement userUpdated
+  }
+
+  @override
+  void usersLoaded() {
+    // TODO: implement usersLoaded
+  }
 }
--- lib/cubits/userprofile_cubit/user_state.dart
+++ lib/cubits/userprofile_cubit/user_state.dart
@@ -1,11 +1,19 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter_chat_app/data_repo/models/message_model.dart';
 
-abstract class UserState extends Equatable {
-  const UserState();
-}
+// abstract class UserState extends Equatable {
+//   const UserState();
+// }
+//
+// class UserInitialState extends UserState {
+//   @override
+//   List<Object> get props => [];
+// }
 
-class UserInitialState extends UserState {
-  @override
-  List<Object> get props => [];
+enum AuthenticateUserState {
+  usersLoaded,
+  userInfoLoaded,
+  userLoadingFailed,
+  noUsers,
+  userUpdated,
+  userDeleted,
 }
