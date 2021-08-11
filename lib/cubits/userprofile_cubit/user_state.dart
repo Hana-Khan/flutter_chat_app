efa8a585b34ce7b753397e54cb9a86936936757f
Hanna Khan <47082795+Hana-Khan@users.noreply.github.com>
2021-08-11 13:04:46 +0500

    1:04PM 11/8/2021 Hina

--- lib/cubits/userprofile_cubit/user_state.dart
+++ lib/cubits/userprofile_cubit/user_state.dart
@@ -1,11 +1,1 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter_chat_app/data_repo/models/message_model.dart';
 
-abstract class UserState extends Equatable {
-  const UserState();
-}
-
-class UserInitialState extends UserState {
-  @override
-  List<Object> get props => [];
-}
